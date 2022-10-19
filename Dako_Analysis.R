3c627c3936c840bea42b6e6e76c20b0bf42015cb
dibamus <isaacwkrone@gmail.com>
2022-10-19 13:31:13 -0700

    Initial commit including all data and scripts needed to produce the Dako paper's analyses & figures.

---
+++ Dako_Analysis.R
@@ -0,0 +1,95 @@
+#Gunung Dako Analysis
+setwd("path to your working directory with the scripts and data")
+
+# Load in these libraries
+library("tidyverse")
+library("readxl")
+library("vegan")
+library("elevatr")
+library("reshape2")
+library("leaflet")
+library("cowplot")
+library("sf")
+
+#source in the functions necessary for analysis
+source("checklistFunctions.R")
+
+##SETUP - color palette for plotting
+colors <- setNames(c("#B5BA72","#a8ccde","#3b5374","#586A6A"),
+                   nm = c("frog","lizard","snake","cumulative"))
+
+#### STEP 1 - Data Import/Cleanup ####
+df <- read_excel("DakoCatalog.xlsx",sheet = 1) %>% mutate(binom = "Genus_")
+
+colnames(df) <- sub(" ","_",colnames(df))
+colnames(df) <- gsub("[()]","",colnames(df))
+
+df <- df %>% mutate(binom = Genus_species) 
+
+#Remove specimens with uncertain affinities
+df <- df %>%
+  filter(Genus_species != "Sphenomorphus \"nigrilabris\" indet.") %>% #exclude indeterminate Sphenomorphus specimens
+  filter(JAM_Number != 16552) %>% #exlude unwanted specimen in city
+  filter(JAM_Number != 16535) #exclude turtle (Leucocephalon yunwoi)
+
+#### STEP 2 - Add groups/Look up Elevation####
+
+cutoffs <- c(700,1400) #- cutoff values for elevational bands
+
+df <- df %>% addGroups() %>% elevBands(b1max = cutoffs[1], b2max = cutoffs[2])
+
+#put specimens missing coords in the middle elevational band
+df$eband[which(df$JAM_Number %in% c(15998,16201,16409))] <- "700-1400m" 
+
+#### STEP 3 - Generate Plots ####
+accumulationPlots <- accCurve(df,colors)
+elevationPlot <- plotElev(df,colors)
+
+alignedAccumulation <- align_plots(plotlist = list(accumulationPlots$taxonacc + theme(legend.position = "none"),
+                                                   accumulationPlots$bandacc + theme(legend.position = "none")),
+                                   align = "hv",
+                                   axis = "blr")
+
+accumulation <- plot_grid(alignedAccumulation[[1]], alignedAccumulation[[2]], 
+                          rel_widths = c(3,2),
+                          rel_heights = c(2,1))
+
+#### STEP 4 - Generate Tables ####
+
+#Table 1
+t1 <- speciesTable(filter(df))
+
+#Table 2
+t2 <- missingSpecies(df) 
+
+#### STEP 5 - Calculate beta diversity for elevational bands #####
+
+beta <- betadiver(veganize(df, elevation = TRUE))
+
+#### STEP 6 - Leaflet Interactive Map #####
+elevationcolors <- colorFactor(c("#A8CCDE","#4E819A","#3B5374"), df$eband)
+
+dakoPoly <- st_read("shp_0/WDPA_WDOECM_Sep2022_Public_555571285_shp-polygons.shp")
+# 
+
+map <- df %>% 
+  leaflet() %>%
+  addProviderTiles(providers$Esri.WorldGrayCanvas) %>% 
+  addProviderTiles(providers$Esri.WorldShadedRelief, options = c(opacity = 0.3)) %>% # using ESRI World Topo for the background map tiles
+  addPolygons(data = dakoPoly$geometry,
+              color = "#586A6A", weight = 2,
+              fillColor = "#B5BA72",
+              smoothFactor = 0.2,
+              opacity = 1.0, fillOpacity = 0.5) %>%
+  
+  addCircleMarkers(label = paste(df$JAM_Number,"-",df$Genus_species),
+                   color = ~elevationcolors(df$eband),
+                   popup = paste(df$JAM_Number,"-",df$Genus_species,"<br>",
+                                 df$Collection_Date,"<br>",
+                                 "Lat =",df$Latitude,"  ","Lon =",df$Longitude,"  ","Elev =",df$Elevation,"<br>",
+                                 df$Habitat),
+                   radius=4) %>% #this adds in markers. on mouse-over, it will say their JAM ID and the binomial
+  
+  setView(lat = df$Latitude[10], lng = df$Longitude[10],zoom = 11) %>%
+  addScaleBar(position = "bottomleft",
+              options= scaleBarOptions(metric = TRUE))
